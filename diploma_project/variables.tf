a41aea81133b999fcd5a6ede8d8ef69998bb58b3
Artem-K16git <bigbox116@gmail.com>
2025-11-11 16:55:33 +0300

    Update variables.tf

--- diploma_project/variables.tf
+++ diploma_project/variables.tf
@@ -1,13 +1,13 @@
 variable "yc_cloud_id" {
   type        = string
   description = "Yandex Cloud ID"
-  default     = "b1g1i4af7bothunbpi4u"
+  default     = "your_yc_cloud_id"
 }
 
 variable "yc_folder_id" {
   type        = string
   description = "Yandex Cloud Folder ID"
-  default     = "b1gr3bbitfk3mr755b15"
+  default     = "your_yc_folder_id"
 }
 
 variable "vm_username" {
@@ -40,5 +40,5 @@ variable "ssh_key_path" {
 variable "ssh_public_key" {
   description = "SSH public key for VM access"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFZ8/AszQy1+2a9noIPmdit1SiFB+d/XVPqboBO+CANULgMq11DVFl8pzacj2zyL3REo3C+7tU/qH5hZrtvMWgouTc0jv+U2HvG/g9zzLwO84ZSTOTzfwdtwv4127AuyjD5YDj8edXaXUNqxOFRUj/DL4uK3UdZ9NgJdNkjyHvg3hZ8r2pxicAUDinj2Sv70J2T4k29lPEkKoVchtIcF/YkFLEx4F6QIT7sDWOtxXbyL/CItquAYQwRgSO3yNuJ4M4DZDHtwmPIRdIHHNlGa6Al0l+Jztqf85Hl8MuYFvmbFufTS8Gp4JHfU436T8F4uGBZA7o8QOdR7WR/g/MxMUsAth2EMrNT7oqbe0CEE+Ru7U9PMzta5Ps1eDOBYTkLwwkVqj6fyvpxdzIcgY8dCK6U2CEgtYFalnuCfauswEQS+4HE8phr8m6Uq1ubIJ8SQkS0G7oKb/r761tXI7pqtJyRf6Ar0RqLEBPcERcpq/TYwcQvmdKHdCSdWLwbl68fMs= artem@deb12-vm3"
+  default     = "your_ssh-key.pub"
 }
